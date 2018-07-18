a3e44deca1c0b511b907c104c25e5ea9a829b29a
sandumjacob <sandumjacobpersonal@gmail.com>
2018-07-18 15:06:22 -0500

    Android O update

--- app/src/main/java/com/jacobsandum/inevitable/Receiver.java
+++ app/src/main/java/com/jacobsandum/inevitable/Receiver.java
@@ -3,10 +3,13 @@ package com.jacobsandum.inevitable;
 import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
@@ -24,6 +27,16 @@ public class Receiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         Notification.Builder builder = new Notification.Builder(context);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel channel = new NotificationChannel("InevitableChannel",
+                    "Channel name",
+                    NotificationManager.IMPORTANCE_LOW);
+            channel.setDescription("Channel description");
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId("InevitableChannel");
+        }
         builder.setContentTitle("Remember");
         builder.setContentText("Everyone Dies Eventually");
         builder.setSmallIcon(R.mipmap.ic_launcher);
@@ -31,9 +44,16 @@ public class Receiver extends BroadcastReceiver {
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         //to be able to launch your activity from the notification
         builder.setContentIntent(pendingIntent);
+
         Notification notificationCompat = builder.build();
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
         managerCompat.notify(NOTIFICATION_ID, notificationCompat);
+
+        /*//Notification Channel for Android O
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("InevitableChannel", "Inevitable Channel", NotificationManager.IMPORTANCE_LOW);
+            notificationChannel.setDescription("Notification Channel for Inevitable Notifications");
+        }*/
 
         cancelAlarm(context);
         setAlarm(context);
@@ -48,20 +68,12 @@ public class Receiver extends BroadcastReceiver {
 
         Random r = new Random();
         int ri = r.nextInt(24 - 1) + 1; // Random integer between 12 (inclusive) and 1 (exclusive),
-        int delay = 1000 * 60 * 60 * ri; // Millisec * Second * Minute * hours
-
-        /*Calendar tenPM = Calendar.getInstance();
-        tenPM.set(Calendar.HOUR_OF_DAY, 22);
-
-        Calendar tenAM = Calendar.getInstance();
-        tenAM.set(Calendar.HOUR_OF_DAY, 10);
-        tenAM.set(Calendar.DAY_OF_YEAR, tenPM.get(Calendar.DAY_OF_YEAR)+1);
-
-        Calendar alarmCal = Calendar.getInstance();
-        alarmCal.add(Calendar.HOUR_OF_DAY, ri);*/
+        //int delay = 1000 * 60 * 60 * ri; // Millisec * Second * Minute * hours
+        int delay = 1000 * 3600;
 
         am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + delay, pi); //Delay
         Log.d("Notif", "Delay set to: " +delay/1000/60/60 + " Hours" );
+        Log.d("Notif", "Milli: " + delay);
 
     }
 
